4aa408f8b7235a7ecb1d17edcb38ccdaabe8cf50
Ubuntu <georgidolap@gmail.com>
2025-10-04 06:01:09 +0000

    testing...

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 CI/CD Pipeline with GitHub Actions & Docker 01 & testing live demo"
+    return "🚀 CI/CD Pipeline with GitHub Actions & Docker 01 testingg......"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
